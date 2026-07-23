bc492eecdcd2cf7b412421f5f33fc0177008f6b5
Yaman Raj Singh <yamanrajsingh07@gmail.com>
2026-07-23 23:32:14 +0530

    Initial commit

---
+++ Url/src/test/java/snapLink/Url/UrlApplicationTests.java
@@ -0,0 +1,13 @@
+package snapLink.Url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
